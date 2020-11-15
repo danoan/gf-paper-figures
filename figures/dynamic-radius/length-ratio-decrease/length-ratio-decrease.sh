a45d68d37a9cb1f250974c9e73e297b0d629fad8
DanielMA <danoan2008@gmail.com>
2020-11-15 15:13:56 +0000

    Fix path dynamic-radius

--- figures/dynamic-radius/length-ratio-decrease/length-ratio-decrease.sh
+++ figures/dynamic-radius/length-ratio-decrease/length-ratio-decrease.sh
@@ -3,8 +3,8 @@
 SCRIPT_PATH=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
 PROJECT_PATH=$( cd $SCRIPT_PATH && cd ../../.. && pwd)
 
-LEGNTH_RATIO_APP="${PROJECT_PATH}/cmake-build-release/figures/length-ratio-decrease/length-ratio-decrease"
-GENERATE_DISK_APP="${PROJECT_PATH}/cmake-build-release/figures/limit-radius/generate-disk"
+LEGNTH_RATIO_APP="${PROJECT_PATH}/cmake-build-release/figures/dynamic-radius/length-ratio-decrease/length-ratio-decrease"
+GENERATE_DISK_APP="${PROJECT_PATH}/cmake-build-release/figures/dynamic-radius/limit-radius/generate-disk"
 GRAPH_FLOW_APP="${PROJECT_PATH}/ext-projects/cmake-build-release/bin/graph-flow-app"
 
 DISK_RADIUS=$1
@@ -17,7 +17,7 @@ mkdir -p ${OUTPUT_FOLDER}
 
 DISK_IMAGE="${OUTPUT_FOLDER}/disk.png"
 ${GENERATE_DISK_APP} "${DISK_RADIUS}" "${DISK_IMAGE}" ${H}
-${GRAPH_FLOW_APP} -S"${DISK_IMAGE}" -h${H} -a0 -b1 -r"${EVOLUTION_RADIUS}" -N0 -O2 -i15 -s ${OUTPUT_FOLDER}/temp
+${GRAPH_FLOW_APP} -S"${DISK_IMAGE}" -h${H} -a0 -b1 -r"${EVOLUTION_RADIUS}" -N0 -O10 -i15 -s ${OUTPUT_FOLDER}/temp
 
 P="0"
 TK2="0"
@@ -34,11 +34,11 @@ do
     echo "$OUT"
     if [ ! "${TK2}" = "0" ]
     then
-        echo $( python -c "print( 'pdiff:', ($LAST_P - $P), 'diffratio:', ($LAST_P - $P)/$TK2 ) " )
+        echo $( python3 -c "print( 'pdiff:', ($LAST_P - $P), 'diffratio:', ($LAST_P - $P)/$TK2 ) " )
     fi
 done
 
-rm -rf ${OUTPUT_FOLDER}/temp
+# rm -rf ${OUTPUT_FOLDER}/temp
 
 #./figures/length-ratio-decrease/length-ratio-decrease.sh 50 0.0625 42 5
 #314.135 0.144922
